d26ff0cc7234369afcff3a45642bfb7345511860
Martino De Ninis <martinodn2004@gmail.com>
2025-11-01 16:41:27 -0300

    Comandos "N" y Comandos con Digitos.

--- src/tp2/mv/operaciones.clj
+++ src/tp2/mv/operaciones.clj
@@ -17,3 +17,22 @@
   (assoc estado
     :ds
     (cons t (:ds estado))))
+
+(defn op-apilar-cero [estado]
+  (assoc estado
+    :ds
+    (cons 0 (:ds estado))))
+
+(defn op-digito [estado valor-digito]
+  (if (empty? (:ds estado))
+
+    estado
+
+    (let [tope-actual (first (:ds estado))
+          resto-pila (rest (:ds estado))
+          nuevo-valor (+ (* tope-actual 10) valor-digito)
+          ]
+      (assoc estado
+        :ds
+
+        (cons nuevo-valor resto-pila)))))
