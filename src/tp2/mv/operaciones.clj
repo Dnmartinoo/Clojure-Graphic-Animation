18c2460030bdac85241b0be78fe73532d4e9edac
Martino De Ninis <martinodn2004@gmail.com>
2025-11-01 16:06:37 -0300

    Comandos "Y", "T"

--- src/tp2/mv/operaciones.clj
+++ src/tp2/mv/operaciones.clj
@@ -7,3 +7,13 @@
 
     (cons x (:ds estado))
     ))
+
+(defn op-apilar-y [estado y]
+  (assoc estado
+    :ds
+    (cons y (:ds estado))))
+
+(defn op-apilar-t [estado t]
+  (assoc estado
+    :ds
+    (cons t (:ds estado))))
