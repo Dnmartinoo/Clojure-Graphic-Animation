a3b884e82f66d6c5303d1aa87ca5e87fd240746c
Martino De Ninis <martinodn2004@gmail.com>
2025-11-01 20:44:04 -0300

    Implementacion de frame->imagen-interna secuencial.

--- test/tp2/render_test.clj
+++ test/tp2/render_test.clj
@@ -1,34 +1,48 @@
+(ns tp2.render-test
+  (:require [clojure.test :refer :all]
+            [tp2.runtime.render :as render])
+  (:import [java.awt.image BufferedImage]))
+
+(defn- rgb->int [r g b]
+  (unchecked-int
+    (-> 0xFF000000
+        (bit-or (bit-shift-left r 16))
+        (bit-or (bit-shift-left g 8))
+        (bit-or b))))
+(def color-azul-int (rgb->int 0 0 255))
+(def color-rojo-int (rgb->int 255 0 0))
+
 (deftest test-render-secuencial
-         (testing "Genera un frame de color sólido (Azul)"
-                  (let [;; ¡CORREGIDO! Este es el código para Azul (R=0, G=0, B=255)
-                        codigo "N0N0N255"
-                        t 0
-                        resultado (render/frame->imagen-interna codigo t)]
+  (testing "Genera un frame de color sólido (Azul)"
+    (let [
+          codigo "N0N0N255"
+          t 0
+          resultado (render/frame->imagen-interna codigo t)]
 
-                    (is (contains? resultado :ok) "El resultado debe ser :ok")
+      (is (contains? resultado :ok) "El resultado debe ser :ok")
 
-                    (let [imagen (:ok resultado)]
-                      (is (instance? BufferedImage imagen) "El resultado debe ser un BufferedImage")
-                      (is (= 256 (.getWidth imagen)))
-                      (is (= 256 (.getHeight imagen)))
-                      (is (= color-azul-int (.getRGB imagen 0 0)))
-                      (is (= color-azul-int (.getRGB imagen 128 128)))
-                      (is (= color-azul-int (.getRGB imagen 255 255))))))
+      (let [imagen (:ok resultado)]
+        (is (instance? BufferedImage imagen) "El resultado debe ser un BufferedImage")
+        (is (= 256 (.getWidth imagen)))
+        (is (= 256 (.getHeight imagen)))
+        (is (= color-azul-int (.getRGB imagen 0 0)))
+        (is (= color-azul-int (.getRGB imagen 128 128)))
+        (is (= color-azul-int (.getRGB imagen 255 255))))))
 
-         (testing "Genera un frame de color sólido (Rojo)"
-                  (let [;; ¡CORREGIDO! Este es el código para Rojo (R=255, G=0, B=0)
-                        codigo "N255N0N0"
-                        t 0
-                        resultado (render/frame->imagen-interna codigo t)]
-                    (is (contains? resultado :ok) "El resultado debe ser :ok")
-                    (let [imagen (:ok resultado)]
-                      (is (= color-rojo-int (.getRGB imagen 100 100))))))
+  (testing "Genera un frame de color sólido (Rojo)"
+    (let [
+          codigo "N255N0N0"
+          t 0
+          resultado (render/frame->imagen-interna codigo t)]
+      (is (contains? resultado :ok) "El resultado debe ser :ok")
+      (let [imagen (:ok resultado)]
+        (is (= color-rojo-int (.getRGB imagen 100 100))))))
 
-         (testing "Propaga el error si un píxel falla"
-                  (let [codigo "P" ; Este queda igual
-                        t 0
-                        resultado (render/frame->imagen-interna codigo t)]
+  (testing "Propaga el error si un píxel falla"
+    (let [codigo "P"
+          t 0
+          resultado (render/frame->imagen-interna codigo t)]
 
-                    (is (contains? resultado :error))
-                    (is (= "Pila vacia" (:error resultado)))))
-         )
+      (is (contains? resultado :error))
+      (is (= "Pila vacia" (:error resultado)))))
+  )
