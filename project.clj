e0357dc6afa2ccc5eda5929d8e51ada92b1e39db
De Ninis Martino <martinodn2004@gmail.com>
2026-07-29 23:27:07 -0300

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,10 +1,13 @@
-(defproject tp2-anim "0.1.0-SNAPSHOT"
-  :description "TP2 Paradigmas - Animación VM - GUI/Observer/Scheduler"
-  :url "https://example.com/tp2"
-  :license {:name "MIT"}
+(defproject clojure-graphic-animation "1.0.0"
+  :description "Functional graphics engine powered by a stack-based virtual machine"
+  :url "https://github.com/Dnmartinoo/Clojure-Graphic-Animation"
+  :license {:name "Eclipse Public License 2.0"}
+
   :min-lein-version "2.9.8"
+
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [seesaw "1.5.0"]]
+
   :main ^:skip-aot tp2.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
